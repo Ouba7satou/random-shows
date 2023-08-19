5a6b34a05c845e48d766c5af12e5f18335c96f6e
Ouba7satou <99183259+Ouba7satou@users.noreply.github.com>
2023-08-19 16:26:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,270 @@
+let arrryOfitems = [
+  "Blue Period",
+  "Bubble",
+  "Castlevania",
+  "Chikyuugai Shounen Shoujo",
+  "Cyberpunk - Edgerunners",
+  "Do It Yourself!!",
+  "Eiga Daisuki Pompo-san",
+  "Fate Grand Order",
+  "Fate Stay Night Heaven's Feel",
+  "Fuuto Tantei",
+  "Gintama the Final ",
+  "Haikyuu!! S04",
+  "Joran The Princess of Snow and Blood",
+  "Josee to Sakana-tachi",
+  "Kaizoku Oujo ",
+  "Kakegurui Twin ",
+  "Komi-san wa, Comyushou desu",
+  "Koroshi Ai ",
+  "Kotarou wa Hitorigurashi",
+  "Kumichou Musume to Sewagakari ",
+  "Megalo Box",
+  "Mob Psycho 100",
+  "Odd Taxi",
+  "One Piece",
+  "Osomatsu-san (Season 3)",
+  "Otherside Picnic ",
+  "Overlord S04 ",
+  "RikeKoi S02",
+  "Romantic Killer",
+  "Sabikui Bisco S01",
+  "Sakura Kakumei - Hana Saku Otome-tachi",
+  "Scissor.Seven",
+  "Shachiku-san wa Youjo Yuurei ni Iyasaretai",
+  "Shadows House ",
+  "Shika no Ou Yuna to Yakusoku no Tabi",
+  "Sonny Boy ",
+  "Spriggan ",
+  "Summer.Ghost",
+  "Vanitas no Karte",
+  "Tomodachi Game",
+  "Tsuki to Laika to Nosferatu",
+  "Uzaki-chan wa Asobitai! ",
+  "Vivy Fluorite Eye's Song",
+  "Words.Bubble.Up.Like.Soda.Pop.Movie",
+  "Yuukoku no Moriarty",
+  "Star.Wars.Visions",
+  "Super Crooks",
+  "  Tengoku Daimakyou ",
+  "Buddy Daddies",
+  "Dr. Stone (Season 3)",
+  " Kono Subarashii Sekai ni Bakuen wo! ",
+  "Bocchi the Rock! ",
+  "Boku ga Aishita + Kimi o Aishita",
+  "Chainsaw Man",
+  "Summer Time Render ",
+  " Fuufu Ijou, Koibito Miman ",
+  "Tomo-chan wa Onnanoko! ",
+  "Blue Lock ",
+  "Otonari no Tenshi-sama ni Itsunomanika Dame Ningen ni Sareteita Ken",
+  "Ai no Utagoe wo Kikasete",
+  "Boku no Kokoro no Yabai Yatsu ",
+  "Kaminaki Sekai no Kamisama ",
+  "Kimetsu no Yaiba",
+  "Kimi wa Houkago Insomnia ",
+  "My Home Hero",
+  "Ousama Ranking ",
+  "Skip to Loafer ",
+  "Sword Art Online Progressive",
+  "Vinland Saga",
+  "Yuusha ga Shinda!",
+  "Natsu e no Tunnel, Sayonara no Deguchi",
+  "Spy x Family ",
+  "Carnival Row S1 + S2",
+  "Designated.Survivor.60.Days",
+  "1987 When The Day Comes (2017)",
+  "Arcane",
+  "Band.Of.Brothers",
+  "Beyond.Evil",
+  "Dexter.New.Blood",
+  "Get Out (2017) ",
+  "I Saw The Devil (2010)",
+  "Memories Of Murder (2003)",
+  "Mouse",
+  "One Sunny Day",
+  "See.S02 +S03",
+  "Somebody",
+  "The Handmaiden (2016)",
+  "The Menu (2022)",
+  "The.Capture.S02",
+  "The.Lost.Room",
+  "Too.Old.to.Die.Young",
+  "Twenty.Five.Twenty.One",
+  "Waraa.Al.Tabee3a",
+  "Weird.Lawyer.Woo.Young.Woo",
+  "Zodiac (2007) ",
+  "I.Am.Mother",
+  "The.Hunt.2012",
+  "7.Prisoners.2021",
+  "Beasts.Clawing.At.Straws.2020",
+  "Call.2020",
+  "Green.Book.2018",
+  "Interstellar.2014",
+  "No.Country.For.Old.Men.2007",
+  "Prisoners.2013",
+  "Recalled.2021",
+  "Red.Notice.2021",
+  "Shutter.Island.2010",
+  "Tenet.2020",
+  "The.Banker.2020",
+  "The.Fear.Of.13.2015",
+  "The.Gangster.The.Cop.The.Devil.2019",
+  "The.Martian.2015",
+  "The.Tomorrow.War.2021",
+  "Wrath.Of.Man.2021",
+];
+
+let container = document.createElement("div");
+container.className = "container";
+container.style.cssText = `
+background: #ddd;
+    padding: 15px;
+    border: 1px solid #009688;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap:10px;
+    border-radius: 4px;`;
+
+for (let i = 0; i < arrryOfitems.length; i++) {
+  let arrryOfitems_div = document.createElement("div");
+  arrryOfitems_div.style.cssText = `border: 1px solid #e91e6373;
+  padding: 8px 20px;
+  border-radius: 4px;
+  background-color: #00bcd44a;
+}`;
+  let div_index = document.createTextNode(`${arrryOfitems[i]}`);
+  arrryOfitems_div.appendChild(div_index);
+  container.appendChild(arrryOfitems_div);
+}
+document.body.appendChild(container);
+//random item filed
+let fieldRandom = document.createElement("div");
+fieldRandom.textContent = `Get A Random Show`;
+fieldRandom.style.cssText = `background-color: #f44336e0;
+margin: 20px auto;
+width: fit-content;
+padding: 10px 20px;
+border-radius: 10px;
+color: black;
+font-size: 22px;
+letter-spacing: 2px;
+font-weight: lighter;
+}`;
+document.body.appendChild(fieldRandom);
+
+//button random
+let btn = document.createElement("button");
+btn.textContent = "Choose";
+document.body.appendChild(btn);
+btn.style.cssText = `margin: 20px auto;
+display: block;
+padding: 10px 20px;
+font-size: 23px;
+letter-spacing: 5px;
+border: 2px solid #00BCD4;
+border-radius: 4px;
+background-color: #00BCD4;
+color: white;
+cursor: pointer;
+}`;
+//
+let newArr = [];
+
+let container_2 = document.createElement("div");
+container_2.className = "show_number";
+container_2.style = `display: flex;
+flex-wrap: wrap;
+background: rgb(221, 221, 221);
+border: 1px solid rgb(0, 150, 136);
+border-radius: 6px;
+gap: 20px;
+padding: 15px 10px;
+justify-content: space-between;
+align-items: center;
+`;
+
+let counter_left = document.createElement("div");
+let show_watch = document.createElement("div");
+if (window.localStorage.getItem("Watched")) {
+  newArr = JSON.parse(localStorage.getItem("Watched"));
+}
+
+btn.onclick = () => {
+  let remainingItems = arrryOfitems.filter((item) => !newArr.includes(item));
+
+  if (remainingItems.length === 0) {
+    return (fieldRandom.textContent = `All the list has been watched`);
+  }
+
+  let random = Math.floor(Math.random() * remainingItems.length);
+  let randomItem = remainingItems[random];
+
+  newArr.push(randomItem);
+  fieldRandom.textContent = randomItem;
+  localStorage.setItem("Watched", JSON.stringify(newArr));
+  counter_left.textContent = `Number Of Shows left: ${
+    arrryOfitems.length - newArr.length
+  }`;
+
+  show_watch.textContent = `Wathed Shows: ${newArr.length}`;
+};
+
+counter_left.textContent = `Number Of Shows left: ${
+  arrryOfitems.length - newArr.length
+}`;
+counter_left.style = `background: #009688;
+width: fit-content;
+padding: 10px 20px;
+border: 2px solid #009688;
+border-radius: 25px;
+font-size: 20px;`;
+
+let counter_number = document.createElement("div");
+counter_number.textContent = `Show: ${arrryOfitems.length}`;
+counter_number.style = `background: #FF9800;
+width: fit-content;
+padding: 10px 20px;
+border: 2px solid #FF9800;
+border-radius: 25px;
+font-size: 20px;`;
+container_2.appendChild(counter_number);
+container_2.appendChild(counter_left);
+
+show_watch.textContent = `Wathed Shows: ${newArr.length}`;
+show_watch.style.cssText = `background: #4CAF50;
+width: fit-content;
+padding: 10px 20px;
+border: 2px solid #4CAF50;
+border-radius: 25px;
+font-size: 20px;`;
+
+container_2.appendChild(show_watch);
+let btn_rest = document.createElement("button");
+btn_rest.textContent = `Reset`;
+btn_rest.style = `
+display: block;
+padding: 10px 20px;
+font-size: 23px;
+letter-spacing: 1px;
+border: none;
+border-radius: 4px;
+background-color: rgb(212 0 0);
+color: white;
+cursor: pointer;
+`;
+btn_rest.onclick = () => {
+  localStorage.clear("Watched");
+  newArr = [];
+  fieldRandom.textContent = `Get A Random Show`;
+  counter_left.textContent = `Number Of Shows left: ${
+    arrryOfitems.length - newArr.length
+  }`;
+  counter_number.textContent = `Show: ${arrryOfitems.length}`;
+  show_watch.textContent = `Wathed Shows: ${newArr.length}`;
+};
+container_2.appendChild(btn_rest);
+
+document.body.appendChild(container_2);
