502ec17c1710e3e3b7a75c961b2b0984754e5fa2
Ouba7satou <99183259+Ouba7satou@users.noreply.github.com>
2024-01-06 17:38:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -180,7 +180,7 @@ let arrryOfitems = [
   "HIGH CARD",
   "Fumetsu no Anata e S01+S02",
   "Kaiko Sareta Ankoku Heishi (30-dai) no Slow na Second Life",
-  "Kaguya-sama wa Kokurasetai","Ai no Utagoe o Kikasete","Natsu e no Tunnel, Sayonara no Deguchi"
+  "Kaguya-sama wa Kokurasetai","Ai no Utagoe o Kikasete","Natsu e no Tunnel, Sayonara no Deguchi",
 ];
 
 let container = document.createElement("div");
